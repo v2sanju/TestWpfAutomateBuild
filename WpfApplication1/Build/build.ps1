c0ed855c3250156a8e3ee6852bc7bcc49d829ce2
v2sanju <sanju.khattar@v2solutions.com>
2016-12-14 13:10:26 +0530

    some changes in build script

--- WpfApplication1/Build/build.ps1
+++ WpfApplication1/Build/build.ps1
@@ -1,8 +1,12 @@
 ﻿cls
 
+# '[p]sake' is the same as 'psake' but $Error is not polluted
 remove-module [p]sake
 
-Import-Module ..\packages\psake.*\tools\psake.psm1
+# find psake's path
+$psakeModule = (Get-ChildItem ("..\Packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+
+Import-Module $psakeModule
 
 Invoke-psake -buildFile .\default.ps1 -taskList Test -framework 4.0 -parameters @{"solutionFile" = "..\WpfApplication1.sln"}
 #Invoke-psake -buildFile .\default.ps1 -taskList Test -parameters @{"solutionFile" = "D:\Projects\STR\GIT\STR-Refresh\src\SelecT.DMS\SelecT.DMS.sln"}
