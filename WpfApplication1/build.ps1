5cd2cc4492258a4f7c81fda5c93385a7d8270fbe
v2sanju <sanju.khattar@v2solutions.com>
2016-12-14 13:38:31 +0530

    changes..

--- WpfApplication1/build.ps1
+++ WpfApplication1/build.ps1
@@ -4,9 +4,9 @@
 remove-module [p]sake
 
 # find psake's path
-$psakeModule = (Get-ChildItem ("..\Packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+$psakeModule = (Get-ChildItem (".\Packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1
 
 Import-Module $psakeModule
 
-Invoke-psake -buildFile .\default.ps1 -taskList Test -framework 4.0 -parameters @{"solutionFile" = "..\WpfApplication1.sln"}
+Invoke-psake -buildFile .\Build\default.ps1 -taskList Test -framework 4.0 -parameters @{"solutionFile" = "..\WpfApplication1.sln"}
 #Invoke-psake -buildFile .\default.ps1 -taskList Test -parameters @{"solutionFile" = "D:\Projects\STR\GIT\STR-Refresh\src\SelecT.DMS\SelecT.DMS.sln"}
